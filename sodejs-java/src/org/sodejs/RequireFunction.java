ceedc36849619ff7c625a8ab065b37284ddece7d
sungon@gmail.com <sungon@gmail.com@a409603c-2190-11df-99f8-b513f4f1e6c6>
2010-02-26 16:01:22 +0000

    js interpreter not dependant on the servelet

--- sodejs-java/src/org/sodejs/RequireFunction.java
+++ sodejs-java/src/org/sodejs/RequireFunction.java
@@ -8,16 +8,16 @@ import org.mozilla.javascript.Scriptable;
 public class RequireFunction extends BaseFunction {
 	private static final long serialVersionUID = 1L;
 
-	private final MainServlet servlet;
+	private final Js js;
 	
-	public RequireFunction(MainServlet servlet) {
-		this.servlet = servlet;
+	public RequireFunction(Js js) {
+		this.js = js;
 	}
 	
 	@Override
 	public Object call(Context cx, Scriptable scope, Scriptable thisObj, Object[] args) {
 		String moduleName = (String) FunctionObject.convertArg(cx, scope, args[0], FunctionObject.JAVA_STRING_TYPE);
-		return servlet.require(moduleName);
+		return js.require(moduleName);
 	}
 
 	@Override
---
+++ sodejs-java/src/org/sodejs/servlet/DynamicConfiguration.java
@@ -0,0 +1,7 @@
+package org.sodejs.servlet;
+
+import org.sodejs.JsConfiguration;
+
+public class DynamicConfiguration extends JsConfiguration {
+	public String pathSpec;
+}
